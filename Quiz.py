73366174f18a849c6a1e6725409a6d22e4a6c548
Travis Logan <c1014427@newcastle.ac.uk>
2022-09-23 12:30:23 +0100

    refactored the initial code to clean-up

--- Quiz.py
+++ Quiz.py
@@ -1,24 +1,18 @@
 def get_input():
-    answer = input("When was the known use of the word 'quiz'? ")
 
-    if answer == "1781":
-        print("Correct!")
-    else:
-        print(f"The answer is '1781', not {answer!r}")
+    QUESTIONS = [
+        ("When was the known use of the word 'quiz'", "1781"),
+        ("What Language was this program built in", "Python"),
+        ("When was Travis Born", "2002")
+    ]
 
-    answer = input("What Language was this program built in? ")
+    for question, correct_answer in QUESTIONS:
+        answer = input(f"{question}? ")
 
-    if answer == "Python":
-        print("Correct")
-    else:
-        print(f"This Program was built in 'Python', not {answer!r}")
-
-    answer = input("When was Travis Born?")
-
-    if answer == "2002":
-        print("Correct!")
-    else:
-        print(f"Travis was born in '2002', not in {answer!r}")
+        if answer == correct_answer:
+            print("Correct!")
+        else:
+            print(f"The answer is {correct_answer!r}, not {answer!r}")
 
 
 if __name__ == '__main__':
